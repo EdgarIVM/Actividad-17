4e202d9d4cfb391169198e06ef7ecd2c9aa89c7e
EdgarIVM <edgar.vazquez4084@alumnos.udg.mx>
2020-11-28 22:28:52 -0600

    Actividad 17

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  peliculas=[];/*
+    {
+      titulo: "John Wick",
+      anio: 2014,
+      director: "Chad Stahelski",
+      imagen: "https://www.thevocket.com/app/uploads/2018/11/John-Wick-3-e1543206506101.jpg"
+    },
+    {
+      titulo: "Matrix",
+      anio: 1999,
+      director: "Hermanas Wachosky",
+      imagen: "https://pics.filmaffinity.com/Matrix-155050517-mmed.jpg"
+    }
+  ];*/
 
-  constructor() {}
+  constructor(private peliculasService: PeliculasService) {}
+
+  ngOnInit(){
+    this.peliculasService.peliculas.subscribe(peliculas => {
+      this.peliculas = peliculas;
+    });
+    //this.peliculasService.getPeliculas();
+  }
 
 }
